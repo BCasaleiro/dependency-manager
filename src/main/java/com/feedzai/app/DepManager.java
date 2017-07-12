483b2e909c28676e76a71d922b79a7144819c37e
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2017-07-12 13:53:30 +0100

    changed from set to hashtable. added service instantiation.

--- src/main/java/com/feedzai/app/DepManager.java
+++ src/main/java/com/feedzai/app/DepManager.java
@@ -3,10 +3,8 @@ package com.feedzai.app;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 import java.io.IOException;
 
@@ -16,17 +14,33 @@ import java.io.IOException;
  */
 public class DepManager
 {
-    private final static String FILE = "dependencies.csv";
-    private final static String DELIMITER = " ";
-    private static final Set<String> AVAILABLE_SERVICES = new HashSet<String>(Arrays.asList( new String[] {"a","b","c","d"} ));
+    private static final String FILE = "dependencies.csv";
+    private static final String DELIMITER = " ";
+    private static final Hashtable<String, Integer> AVAILABLE_SERVICES = new Hashtable<String, Integer>() {{
+        put("a", 1);
+        put("b", 2);
+        put("c", 3);
+        put("d", 4);
+    }};
 
     /**
     * Basic Dependency Manager Constructor
     */
     public DepManager() { }
 
+    private ArrayList<Service> instantiateServices () {
+        ArrayList<Service> services = new ArrayList<Service>();
+        services.add(new ServiceA());
+        services.add(new ServiceB());
+        services.add(new ServiceC());
+        services.add(new ServiceD());
+
+        return services;
+    }
+
     /**
     * Load dependencies into
+    * @return      if there are problems in the file
     */
     private Boolean loadDependencies() {
         FileReader fr = null;
@@ -41,7 +55,7 @@ public class DepManager
             while ((line = br.readLine()) != null) {
                 dependency = line.split(DELIMITER);
 
-                if ( AVAILABLE_SERVICES.contains(dependency[0]) && AVAILABLE_SERVICES.contains(dependency[1]) ) {
+                if ( AVAILABLE_SERVICES.containsKey(dependency[0]) && AVAILABLE_SERVICES.containsKey(dependency[1]) ) {
 
                 } else {
                     return false;
@@ -88,7 +102,10 @@ public class DepManager
         DepManager dm = new DepManager();
 
         if (dm.loadDependencies()) {
-
+            System.out.println("[DEBUG] Dependencies Loaded.");
+            ArrayList services = instantiateServices();
+            System.out.println("[DEBUG] Instantiated Services.");
+            
         } else {
             System.out.println("[ERROR] Invalid dependencies file.");
         }
