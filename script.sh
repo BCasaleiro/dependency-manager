464a5f78e6ae1dd6fd1f1b4af1ab370292b2841c
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2017-07-12 10:22:34 +0100

    initial commit

---
+++ script.sh
@@ -0,0 +1,2 @@
+mvn package
+java -cp target/dependency-manager-1.0-SNAPSHOT.jar com.feedzai.app.DepManager
---
+++ src/main/java/com/feedzai/app/DepManager.java
@@ -0,0 +1,13 @@
+package com.feedzai.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class DepManager 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
